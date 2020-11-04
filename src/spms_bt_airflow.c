f2e22898f3f2bb515fa6a8d35768155737052573
Tijntj3 <tijntj3@gmail.com>
2020-11-04 14:11:22 +0100

    Added write to battery voltage for demo purposes

--- src/spms_bt_airflow.c
+++ src/spms_bt_airflow.c
@@ -14,12 +14,28 @@ static ssize_t read_callback(struct bt_conn *conn, const struct bt_gatt_attr *at
 		sizeof(advData));
 }
 
+static ssize_t write_callback(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+			const void *buf, uint16_t len, uint16_t offset,
+			uint8_t flags)
+{
+	uint8_t *value = attr->user_data;
+
+	if (offset + len > sizeof(advData)) {
+		return BT_GATT_ERR(BT_ATT_ERR_INVALID_OFFSET);
+	}
+
+    memcpy(value + offset, buf, len);
+    advData[10] = value[0];
+    
+	return len;
+}
+
 BT_GATT_SERVICE_DEFINE(airflow_service,
 	BT_GATT_PRIMARY_SERVICE(&airflow_service_uuid),
 		BT_GATT_CHARACTERISTIC(&airflow_characteristic_uuid.uuid,
-			BT_GATT_CHRC_READ,
-			BT_GATT_PERM_READ,
-			read_callback, NULL, advData),
+			BT_GATT_CHRC_READ | BT_GATT_CHRC_WRITE,
+			BT_GATT_PERM_READ | BT_GATT_PERM_WRITE,
+			read_callback, write_callback, advData),
 );
 
 int bt_init_airflow(uint8_t ms_size, uint8_t ms_adtype, uint16_t id)
@@ -40,7 +56,7 @@ int bt_update_airflow(spms_airflow_local* airflowMem, uint8_t status)
     advData[7] = (uint8_t)(airflowMem->humi);
     advData[8] = (uint8_t)(airflowMem->pres >> 8);
     advData[9] = (uint8_t)(airflowMem->pres);
-    advData[10] = airflowMem->batt;
+    //advData[10] = airflowMem->batt;
     advData[11] = status;
     advData[12] = (uint8_t)(airflowMem->airf >> 8);
     advData[13] = (uint8_t)(airflowMem->airf);
